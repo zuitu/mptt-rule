63c56fa5b83f5f9b00d58cd25c18aba9c30ebef5
caoxiaoqing <xiaoqing.cao@wenba100.com>
2017-02-14 16:07:31 +0800

    init

---
+++ src/RuleOp.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *
+ */
+namespace Zuitu\Rule;
+
+class RuleOp {
+    protected $nameList = null;
+
+    public function init(array $nameList) {
+        $this->nameList = $nameList;
+        return $this;
+    }
+
+    public function calc($op, $name, $value) {
+        return $this->$op($name, $value);
+    }
+
+    protected function neq($name, $value) {
+        $my_value = $this->nameList[$name];
+        $ret = false;
+        if ($my_value != $value) {
+            $ret = true;
+        }
+        return $ret;
+    }
+
+    protected function eq($name, $value) {
+        $my_value = $this->nameList[$name];
+        $ret = false;
+        if ($my_value == $value) {
+            $ret = true;
+        }
+        return $ret;
+    }
+
+    protected function gt($name, $value) {
+        $my_value = $this->nameList[$name];
+        $ret = false;
+        if ((int) $my_value > (int) $value) {
+            $ret = true;
+        }
+        return $ret;
+    }
+
+    protected function egt($name, $value) {
+        $my_value = $this->nameList[$name];
+        $ret = false;
+        if ((int) $my_value >= (int) $value) {
+            $ret = true;
+        }
+        return $ret;
+    }
+
+    protected function lt($name, $value) {
+        $my_value = $this->nameList[$name];
+        $ret = false;
+        if ((int) $my_value < (int) $value) {
+            $ret = true;
+        }
+        return $ret;
+    }
+
+    protected function elt($name, $value) {
+        $my_value = $this->nameList[$name];
+        $ret = false;
+        if ((int) $my_value <= (int) $value) {
+            $ret = true;
+        }
+        return $ret;
+    }
+
+    protected function btw($name, $value) {
+        $my_value = $this->nameList[$name];
+        $ret = false;
+        list($min, $max) = explode(',', $value);
+        if ((int) $my_value >= (int) $min && (int) $my_value <= (int) $max) {
+            $ret = true;
+        }
+        return $ret;
+    }
+
+    protected function nbtw($name, $value) {
+        $my_value = $this->nameList[$name];
+        $ret = false;
+        list($min, $max) = explode(',', $value);
+        if ((int) $my_value <= (int) $min || (int) $my_value >= (int) $max) {
+            $ret = true;
+        }
+        return $ret;
+    }
+
+    public function __call($name, $args) {
+        $ret = false;
+        if (1 == ($args[3] - $args[2] + 1) / 2 + $args[4] - 1) {
+            $ret = true;
+        }
+        return $ret;
+    }
+}
